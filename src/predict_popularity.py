bd4c9682d483a603bca9e854f8cca6b44de43306
deed1502 <danieleduardo1502@gmail.com>
2026-01-08 20:49:56 -0400

    resolver rutas relativas usando os.path

--- src/predict_popularity.py
+++ src/predict_popularity.py
@@ -1,10 +1,45 @@
 import pandas as pd
 import joblib
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+data_path = os.path.join(base_dir, '..', 'data', 'processed', 'spotify_total.csv')
+
+model_path = os.path.join(base_dir, 'predictor_model.pkl')
+columns_path = os.path.join(base_dir, 'model_columns.pkl')
+
+def train_model():
+    """Función para entrenar el modelo si no existe."""
+    if not os.path.exists(data_path):
+        print(f"Error: No se encuentra el dataset en {data_path}")
+        return None, None
+    
+    df = pd.read_csv(data_path, index_col='track_id')
+    X = df.drop(['track_name', 'artist_name', 'track_popularity'], axis=1)
+    y = df['track_popularity']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    modelo = RandomForestRegressor(n_estimators=100, random_state=42)
+    modelo.fit(X_train, y_train)
+    predicciones = modelo.predict(X_test)
+    error = mean_absolute_error(y_test, predicciones)
+    r2 = r2_score(y_test, predicciones)
+
+    print(f'El modelo se equivoca, en promedio, por {error:.2f} puntos de popularidad.')
+    print(f'Precisión del modelo (R2): {r2:.2f}')
+    
+    joblib.dump(modelo, model_path)
+    joblib.dump(X.columns.tolist(), columns_path)
+
+    print('Modelo guardado')
+    return modelo, X.columns.tolist()
 
 def predict_popularity(datos_nueva_cancion):
-    modelo_cargado = joblib.load('../src/predictor_model.pkl')
-    columnas = joblib.load('../src/model_columns.pkl')
+    modelo_cargado = joblib.load(model_path)
+    columnas = joblib.load(columns_path)
     
     entrada = pd.DataFrame([datos_nueva_cancion], columns=columnas)
     resultado = modelo_cargado.predict(entrada)
@@ -13,12 +48,16 @@ def predict_popularity(datos_nueva_cancion):
 
 def load_model_and_predict():
 
-    if not os.path.exists('src/predictor_model.pkl'):
-        print('Error: No se encuentra el modelo guardado. Primero ejecute el entrenamiento.')
-        return
-
-    modelo = joblib.load('src/predictor_model.pkl')
-    columnas = joblib.load('src/model_columns.pkl')
+    if os.path.exists(model_path) and os.path.exists(columns_path):
+        modelo = joblib.load(model_path)
+        columnas = joblib.load(columns_path)
+    else:
+        answer = input('\nNo se encontró el modelo. ¿Desea entrenarlo? (S/N)\n')
+        if answer.upper() == 'S':
+            modelo, columnas = train_model()
+        else:
+            return
+            
 
     print('--- Spotify Popularity Predictor ---')
     print('Introduce los datos de la canción para calcular su popularidad estimada\n')
@@ -37,10 +76,25 @@ def load_model_and_predict():
         
         print(f'\nEl modelo estima que la canción tendrá una popularidad de {prediccion:.2f}/100')
         print('Nota: El margen de error promedio del modelo es de +/- 8.02 puntos')
+        input('Presione enter para continuar')
 
     except ValueError:
         print('Error: Por favor, introduce solo valores numéricos válidos.')
 
 if __name__ == '__main__':
-    load_model_and_predict()
+    
+    print('--- Spotify Popularity Predictor ---')
+    while True:
+        print('Bienvenido a Spotify Popularity Predictor')
+        print('1. Entrenar modelo\n2. Hacer predicción\n0. Salir')
+        option = input('Seleccione la opción que desea ejecutar: ')
+        if option == '1':
+            train_model()
+        elif option == '2':
+            load_model_and_predict()
+        elif option == '0':
+            print('¡Hasta luego!')
+            break
+        else:
+            print('Por favor seleccione una opción válida')
 
